392e6d678b45020983731e4e8508e3c5c0708c70
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-06-04 13:18:01 +0300

    Update CreateAnnouncement.jsx

--- src/components/client/CreateAnnouncement.jsx
+++ src/components/client/CreateAnnouncement.jsx
@@ -1,17 +1,23 @@
-import SendIcon from "@mui/icons-material/Send";
 import Autocomplete from "@mui/material/Autocomplete";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { useEffect } from "react";
+import {
+  useStep,
+  usePersonalDetails,
+  useAnnouncement,
+} from "../../context/client/ClientProvider";
+import { STEPPER_ACTIONS } from "../../context/actions/client/stepperActions";
+import { ANNOUNCEMENT_ACTIONS } from "../../context/actions/client/annoucementActions";
 const outer = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  height: "100vh",
   maxWidth: "600px",
   margin: "0 auto",
 };
@@ -22,84 +28,94 @@ const radioStations = [
   { label: "Lira FM", value: "93.7" },
   { label: "Radio Rhino", value: "92.7" },
 ];
+
+const CATEGORIES = [
+  { label: "Death", value: "Death" },
+  { label: "Business", value: "Business" },
+  { label: "Corporate", value: "Corporate" },
+];
 function CreateAnnouncement() {
-  const [name, setName] = useState("");
-  const [nameError, setNameError] = useState(false);
+  const { personalDetails } = usePersonalDetails();
+  const { stepDispatch } = useStep();
+  const { announcementDetails, announcementDetailsDispatch } =
+    useAnnouncement();
 
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [phoneNumberError, setPhoneNumberError] = useState(false);
-
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState(announcementDetails.message);
   const [messageError, setMessageError] = useState(false);
 
-  const [radioStation, setRadioStation] = useState("");
+  const [numberOfTimes, setNumberOfTimes] = useState(
+    announcementDetails.numberOfTimes
+  );
+  const [numberOfTimesError, setNumberOfTimesError] = useState(false);
+
+  const [dateToBroadcast, setDateToBroadcast] = useState(
+    announcementDetails.dateToBroadcast
+  );
+  const [dateToBroadcastError, setDateToBroadcastError] = useState(false);
+
+  const [radioStation, setRadioStation] = useState(
+    announcementDetails.radioStation
+  );
   const [radioStationError, setRadioStationError] = useState(false);
+
+  const [category, setCategory] = useState(announcementDetails.category);
+  const [categoryError, setCategoryError] = useState(false);
 
   const navigate = useNavigate();
 
+  const currentDate = new Date().toISOString().split("T")[0];
+
+  const handleBack = (e) => {
+    e.preventDefault();
+    stepDispatch({ type: STEPPER_ACTIONS.STEP, activeStep: 0 });
+    navigate("/client/personal-details");
+  };
+
   const handleCreateAnnouncement = (e) => {
     e.preventDefault();
-    if (name === "") {
-      setNameError(true);
-      return;
-    }
-
-    if (phoneNumber === "") {
-      setPhoneNumberError(true);
-      return;
-    }
-
     if (message === "") {
       setMessageError(true);
       return;
     }
-
-    if (radioStation === "") {
+    if (numberOfTimes === "") {
+      setNumberOfTimesError(true);
+      return;
+    }
+    if (dateToBroadcast === "") {
+      setDateToBroadcastError(true);
+      return;
+    }
+    if (category === null) {
+      setCategoryError(true);
+      return;
+    }
+    if (radioStation === null) {
       setRadioStationError(true);
       return;
     }
-    console.log(radioStation);
-    navigate("/client/summarize-announcement");
+    stepDispatch({ type: STEPPER_ACTIONS.STEP, activeStep: 2 });
+    announcementDetailsDispatch({
+      type: ANNOUNCEMENT_ACTIONS.ADD_DETAILS,
+      announcementDetails: {
+        message,
+        numberOfTimes,
+        dateToBroadcast,
+        category,
+        radioStation,
+      },
+    });
+    navigate("/client/checkout");
   };
+
+  useEffect(() => {
+    if (personalDetails.name === "" || personalDetails.phoneNumber === "") {
+      navigate("/client/personal-details");
+    }
+  }, [navigate, personalDetails.name, personalDetails.phoneNumber]);
   return (
     <div>
       <Box style={outer} component="form">
         <Box p={1} width="90%">
-          <TextField
-            label="First Name"
-            size="small"
-            fullWidth
-            margin="normal"
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-              setNameError(false);
-            }}
-            helperText={nameError ? "Name is required" : ""}
-            error={nameError}
-          />
-          <TextField
-            label="Phone Number"
-            size="small"
-            type="number"
-            fullWidth
-            margin="normal"
-            value={phoneNumber}
-            onChange={(e) => {
-              setPhoneNumber(e.target.value);
-              setPhoneNumberError(false);
-            }}
-            helperText={phoneNumberError ? "Phone number is required" : ""}
-            error={phoneNumberError}
-          />
-
-          <TextField
-            label="Email (optional)"
-            size="small"
-            type="email"
-            fullWidth
-            margin="normal"
-          />
           <TextField
             label="Message"
             multiline
@@ -115,12 +131,73 @@ function CreateAnnouncement() {
             helperText={messageError ? "Message is required" : ""}
             error={messageError}
           />
+          <TextField
+            label="Number of times to broadcast"
+            size="small"
+            type="number"
+            fullWidth
+            margin="normal"
+            inputProps={{ min: 1 }}
+            value={numberOfTimes}
+            onChange={(e) => {
+              setNumberOfTimes(e.target.value);
+              setNumberOfTimesError(false);
+            }}
+            helperText={
+              numberOfTimesError
+                ? "Number of times to broadcast is required"
+                : ""
+            }
+            error={numberOfTimesError}
+          />
+          <TextField
+            label="Date to broadcast"
+            size="small"
+            type="date"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            value={dateToBroadcast}
+            onChange={(e) => {
+              setDateToBroadcast(e.target.value);
+              setDateToBroadcastError(false);
+            }}
+            inputProps={{ min: currentDate }}
+            helperText={
+              dateToBroadcastError ? "Date to broadcast is required" : ""
+            }
+            error={dateToBroadcastError}
+          />
+          <Autocomplete
+            sx={{ marginBottom: "20px", marginTop: "20px" }}
+            disablePortal
+            options={CATEGORIES}
+            getOptionLabel={(option) => option.label}
+            onChange={(e, option) => {
+              setCategory(option);
+              setCategoryError(false);
+            }}
+            value={category}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Cateogry"
+                error={categoryError}
+                helperText={categoryError ? "Category is required" : ""}
+              />
+            )}
+          />
 
           <Autocomplete
             sx={{ marginBottom: "20px", marginTop: "20px" }}
             disablePortal
             options={radioStations}
-            onChange={(e, option) => setRadioStation(option.label)}
+            onChange={(e, option) => {
+              setRadioStation(option);
+              setRadioStationError(false);
+            }}
             value={radioStation}
             renderInput={(params) => (
               <TextField
@@ -128,21 +205,30 @@ function CreateAnnouncement() {
                 label="Radio"
                 error={radioStationError}
                 helperText={
-                  radioStationError ? "Radio Station is requireed" : ""
+                  radioStationError ? "Radio Station is required" : ""
                 }
               />
             )}
           />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={(e) => {
-              handleCreateAnnouncement(e);
-            }}
-            endIcon={<SendIcon />}
-          >
-            Submit
-          </Button>
+
+          <Box component="div" display="flex" justifyContent="space-between">
+            <Button
+              variant="outlined"
+              color="primary"
+              startIcon={<ArrowBackIosIcon />}
+              onClick={(e) => handleBack(e)}
+            >
+              Back
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              endIcon={<NavigateNextIcon />}
+              onClick={(e) => handleCreateAnnouncement(e)}
+            >
+              Next
+            </Button>
+          </Box>
         </Box>
       </Box>
     </div>
