29a319ab6d041d9a169b7239e0deb5a381a5748c
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-06-06 13:31:06 +0300

    Create MenuAppBar.jsx

---
+++ src/components/Radio/MenuAppBar.jsx
@@ -0,0 +1,49 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuDrawer from "./MenuDrawer";
+import { useState } from "react";
+
+const MenuAppBar = () => {
+  const [state, setState] = useState({
+    left: false,
+  });
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, left: open });
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <MenuDrawer toggleDrawer={toggleDrawer} state={state} />
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Radio Unity
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+export default MenuAppBar;
