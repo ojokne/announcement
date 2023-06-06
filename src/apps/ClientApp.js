b7b040e3759a237965f2f0b5d98ae7b78d5b3b1c
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-06-06 13:29:46 +0300

    Remove ClientProvider

--- src/apps/ClientApp.js
+++ src/apps/ClientApp.js
@@ -1,33 +1,23 @@
-import { Box } from "@mui/material";
-
 import { Route, Routes } from "react-router-dom";
 import CreateAnnouncement from "../components/client/CreateAnnouncement";
 import SummarizeAnnouncement from "../components/client/SummarizeAnnouncement";
 import PersonalDetails from "../components/client/PersonalDetails";
 import ClientWrapper from "../pages/client/ClientWrapper";
-import { ClientProvider } from "../context/client/ClientProvider";
 import Checkout from "../components/client/Checkout";
 
 function ClientApp() {
   return (
-    <ClientProvider>
-      <Box component="div">
-        <Routes>
-          <Route path="/client" element={<ClientWrapper />}>
-            <Route path="personal-details" element={<PersonalDetails />} />
-            <Route
-              path="create-announcement"
-              element={<CreateAnnouncement />}
-            />
-            <Route path="checkout" element={<Checkout />} />
-            <Route
-              path="summarize-announcement"
-              element={<SummarizeAnnouncement />}
-            />
-          </Route>
-        </Routes>
-      </Box>
-    </ClientProvider>
+    <Routes>
+      <Route path="/client" element={<ClientWrapper />}>
+        <Route path="personal-details" element={<PersonalDetails />} />
+        <Route path="create-announcement" element={<CreateAnnouncement />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route
+          path="summarize-announcement"
+          element={<SummarizeAnnouncement />}
+        />
+      </Route>
+    </Routes>
   );
 }
 
