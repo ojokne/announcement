27a06fb69bf9e72b3a773508d6abb9f35257a7f8
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-06-14 18:13:03 +0300

    update routes

--- src/apps/RadioApp.js
+++ src/apps/RadioApp.js
@@ -1,22 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../components/Radio/Dashboard";
-import MenuAppBar from "../components/Radio/MenuAppBar";
-import { Box } from "@mui/material";
 import Death from "../components/Radio/Death";
 import Corporate from "../components/Radio/Corporate";
 import Business from "../components/Radio/Business";
 
 function RadioApp() {
   return (
-    <Box component="div">
-      <MenuAppBar />
-      <Routes>
-        <Route path="/radio" element={<Dashboard />} />
-        <Route path="/radio/death" element={<Death />} />
-        <Route path="/radio/business" element={<Business />} />
-        <Route path="/radio/corporate" element={<Corporate />} />
-      </Routes>
-    </Box>
+    <Routes>
+      <Route path="/radio" element={<Dashboard />} />
+      <Route path="/radio/death" element={<Death />} />
+      <Route path="/radio/business" element={<Business />} />
+      <Route path="/radio/corporate" element={<Corporate />} />
+    </Routes>
   );
 }
 
