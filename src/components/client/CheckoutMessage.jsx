91333871488371ff305f4c18067986ef014d639b
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-06-15 22:12:55 +0300

    Update CheckoutMessage.jsx

--- src/components/client/CheckoutMessage.jsx
+++ src/components/client/CheckoutMessage.jsx
@@ -3,7 +3,7 @@ import Typography from "@mui/material/Typography";
 import ClientMenuAppBar from "./ClientMenuAppBar";
 import CheckIcon from "@mui/icons-material/Check";
 import Button from "@mui/material/Button";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const outer = {
   display: "flex",
@@ -16,6 +16,7 @@ const outer = {
 };
 
 const CheckoutMessage = () => {
+  const { state } = useLocation();
   const navigate = useNavigate();
   const handleTrackStatus = (e) => {
     e.preventDefault();
@@ -25,13 +26,47 @@ const CheckoutMessage = () => {
     <Box component="div">
       <ClientMenuAppBar />
       <Box style={outer}>
-        <CheckIcon color="primary" sx={{ fontSize: 100 }} />
-        <Typography variant="h5" sx={{ fontSize: 18 }}>
-          Your announcement was recieved
-        </Typography>
-        <Typography variant="body2" color="secondary">
-          Please stay tuned
-        </Typography>
+        <Box
+          sx={{
+            padding: 2,
+            margin: 2,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CheckIcon color="primary" sx={{ fontSize: 100 }} />
+          <Typography variant="h5" sx={{ fontSize: 16, textAlign: "center" }}>
+            Your announcement was recieved
+          </Typography>
+          <Typography variant="body2" color="secondary" sx={{ fontSize: 12 }}>
+            Please stay tuned
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            padding: 2,
+            margin: 2,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h5" sx={{ fontSize: 16 }}>
+            {state?.id}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="secondary"
+            sx={{ textAlign: "center", fontSize: 12 }}
+          >
+            Use the reference number to track your announcement
+          </Typography>
+        </Box>
+
         <Button
           variant="outlined"
           color="primary"
