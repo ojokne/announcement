ef406c3ae4fbe92e78da2cd8981f84958bbef438
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-06-15 22:11:39 +0300

    Modify config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getRemoteConfig } from "firebase/remote-config";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBm7ij6_spenYTnZLzKAqJIR8xCIK6i-a4",
-  authDomain: "announcement-5cef0.firebaseapp.com",
-  projectId: "announcement-5cef0",
-  storageBucket: "announcement-5cef0.appspot.com",
-  messagingSenderId: "978497257981",
-  appId: "1:978497257981:web:e9b145f7cb0b29279b5994",
-  measurementId: "G-F1WVFBYH3R",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
+
+export const remoteConfig = getRemoteConfig(app);
