eb566628c283f6999f3035e1618d81930b7644fd
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-06-16 06:14:10 +0300

    Update Death.jsx

--- src/components/Radio/Death.jsx
+++ src/components/Radio/Death.jsx
@@ -4,10 +4,16 @@ import Typography from "@mui/material/Typography";
 import PaidIcon from "@mui/icons-material/Paid";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import CheckIcon from "@mui/icons-material/Check";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 import RadioMenuAppBar from "./RadioMenuAppBar";
 import Stack from "@mui/material/Stack";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { green, orange } from "@mui/material/colors";
+import { useAnnouncement } from "../../context/radio/RadioProvider";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../config/firebase";
 const outer = {
   display: "flex",
   justifyContent: "center",
@@ -19,6 +25,63 @@ const outer = {
 };
 
 const Death = () => {
+  const { announcements } = useAnnouncement();
+
+  // state to open and close backdrop
+  const [open, setOpen] = useState(false);
+
+  // stateto hold amount
+  const [amount, setAmount] = useState(0);
+
+  // state to hold pending  announcements
+  const [pending, setPending] = useState(0);
+
+  // state to hold announcements that have been completed
+  const [complete, setComplete] = useState(0);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setOpen(true);
+    const unsubscribeFromAuth = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        if (announcements.hasOwnProperty("announcements")) {
+          let announcementsArray = Object.values(announcements.announcements);
+          for (let i = 0; i < announcementsArray.length; i++) {
+            setAmount((prev) => prev + announcementsArray[i].amount);
+            if (announcementsArray[i].status === "pending") {
+              setPending((prev) => prev + 1);
+            } else if (announcementsArray[i].status === "complete") {
+              setComplete((prev) => prev + 1);
+            }
+          }
+        } else {
+          navigate("/radio");
+        }
+        setOpen(false);
+      } else {
+        navigate("/login");
+      }
+    });
+
+    return () => {
+      unsubscribeFromAuth();
+    };
+  }, []);
+
+  if (open) {
+    return (
+      <div>
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={open}
+          // onClick={handleClose}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </div>
+    );
+  }
   return (
     <Box component="div">
       <RadioMenuAppBar />
@@ -73,7 +136,7 @@ const Death = () => {
                   fontSize: "1.8rem",
                 }}
               >
-                45,000
+                {amount.toLocaleString("en-US")}
               </Typography>
               <Typography
                 variant="body1"
@@ -121,21 +184,35 @@ const Death = () => {
                   fontSize: "1.8rem",
                 }}
               >
-                26
+                {pending}
               </Typography>
-              <Typography
-                variant="body1"
-                component={RouterLink}
-                to="#"
-                sx={{
-                  paddingTop: 2,
-                  color: "primary.main",
-                  textDecoration: "none",
-                  textAlign: "center",
-                }}
-              >
-                View
-              </Typography>
+              {pending === 0 ? (
+                <Typography
+                  variant="body1"
+                  sx={{
+                    paddingTop: 2,
+                    color: "secondary.main",
+                    textDecoration: "none",
+                    textAlign: "center",
+                  }}
+                >
+                  No announcements
+                </Typography>
+              ) : (
+                <Typography
+                  variant="body1"
+                  component={RouterLink}
+                  to="/radio/death/pending"
+                  sx={{
+                    paddingTop: 2,
+                    color: "primary.main",
+                    textDecoration: "none",
+                    textAlign: "center",
+                  }}
+                >
+                  View
+                </Typography>
+              )}
             </Paper>
             <Paper
               elevation={3}
@@ -169,21 +246,35 @@ const Death = () => {
                   fontSize: "1.8rem",
                 }}
               >
-                10
+                {complete}
               </Typography>
-              <Typography
-                variant="body1"
-                component={RouterLink}
-                to="#"
-                sx={{
-                  paddingTop: 2,
-                  color: "primary.main",
-                  textDecoration: "none",
-                  textAlign: "center",
-                }}
-              >
-                View
-              </Typography>
+              {complete === 0 ? (
+                <Typography
+                  variant="body1"
+                  sx={{
+                    paddingTop: 2,
+                    color: "secondary.main",
+                    textDecoration: "none",
+                    textAlign: "center",
+                  }}
+                >
+                  No announcements
+                </Typography>
+              ) : (
+                <Typography
+                  variant="body1"
+                  component={RouterLink}
+                  to="/radio/death/complete"
+                  sx={{
+                    paddingTop: 2,
+                    color: "primary.main",
+                    textDecoration: "none",
+                    textAlign: "center",
+                  }}
+                >
+                  View
+                </Typography>
+              )}
             </Paper>
           </Box>
         </Box>
