3b1f4dca930605ea03907b1e9834103e1c81afb3
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-06-16 08:17:29 +0300

    Add DeathPending and AnnouncementDetails

--- src/apps/RadioApp.js
+++ src/apps/RadioApp.js
@@ -3,14 +3,20 @@ import Dashboard from "../components/Radio/Dashboard";
 import Death from "../components/Radio/Death";
 import Corporate from "../components/Radio/Corporate";
 import Business from "../components/Radio/Business";
+import DeathPending from "../components/Radio/DeathPending";
+import AnnouncementDetails from "../components/Radio/AnnouncementDetails";
 
 function RadioApp() {
   return (
     <Routes>
       <Route path="/radio" element={<Dashboard />} />
       <Route path="/radio/death" element={<Death />} />
+      <Route path="/radio/death/pending" element={<DeathPending />} />
       <Route path="/radio/business" element={<Business />} />
       <Route path="/radio/corporate" element={<Corporate />} />
+
+      {/* route for announcement details */}
+      <Route path="/radio/announcement/:id" element={<AnnouncementDetails />} />
     </Routes>
   );
 }
