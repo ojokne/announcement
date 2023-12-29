0a4ccbe66d908eee0e2c5354249c55402c40d03b
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-12-29 13:48:30 +0300

    Add Checkout route

--- src/routes/Announcement.jsx
+++ src/routes/Announcement.jsx
@@ -3,14 +3,16 @@ import Wrapper from "../components/Announcement/Wrapper";
 import PersonalDetails from "../components/Announcement/PersonalDetails";
 import AnnouncementDetails from "../components/Announcement/AnnouncementDetails";
 import Review from "../components/Announcement/Review";
+import Checkout from "../components/Announcement/Checkout";
 
 const Announcement = () => {
   return (
     <Routes>
       <Route path="/create" element={<Wrapper />}>
-        <Route path="personal_details" element={<PersonalDetails />} />
+        <Route index element={<PersonalDetails />} />
         <Route path="announcement_details" element={<AnnouncementDetails />} />
-        <Route path="review" element={<Review/>} />
+        <Route path="review" element={<Review />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
