3e805ed46b0758c9ff7d001b1be7058b6fe97d05
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-12-29 06:51:40 +0300

    Rename apps directory to routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./scss/index.scss";
-import Admin from "./apps/Admin.jsx";
-import Announcement from "./apps/Announcement.jsx";
+import Admin from "./routes/Admin.jsx";
+import Announcement from "./routes/Announcement.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
