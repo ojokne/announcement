3d9e67c2e948a29786a3d742cdff682985eb5544
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-12-29 06:49:30 +0300

    Add Announcement component to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./scss/index.scss";
 import Admin from "./apps/Admin.jsx";
+import Announcement from "./apps/Announcement.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Admin />
+      <Announcement />
     </BrowserRouter>
   </React.StrictMode>
 );
