4d01e618e1662d0dbc89a1310d864678a5a7d70f
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-12-29 14:28:17 +0300

    Update route path in Announcement component

--- src/routes/Announcement.jsx
+++ src/routes/Announcement.jsx
@@ -8,7 +8,7 @@ import Checkout from "../components/Announcement/Checkout";
 const Announcement = () => {
   return (
     <Routes>
-      <Route path="/" element={<Wrapper />}>
+      <Route path="/create" element={<Wrapper />}>
         <Route index element={<PersonalDetails />} />
         <Route path="announcement_details" element={<AnnouncementDetails />} />
         <Route path="review" element={<Review />} />
