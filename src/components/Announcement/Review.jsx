b510b1d1370fc423543b8027acbddebb656fc33f
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-12-30 08:36:02 +0300

    Add useEffect hook and handleCheckout function to Review component

--- src/components/Announcement/Review.jsx
+++ src/components/Announcement/Review.jsx
@@ -1,11 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import Spinner from "../Spinner";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../config/firebase";
 
 const Review = () => {
   const navigate = useNavigate();
 
   const [messageOpen, setMessageOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  // retieve form data from local storage
+  const name = localStorage.getItem("name");
+  const contact = localStorage.getItem("contact");
+  const radioName = localStorage.getItem("radioName");
+  const radioId = localStorage.getItem("radioId");
+  const categoryId = localStorage.getItem("categoryId");
+  const categoryName = localStorage.getItem("categoryName");
+  const number = localStorage.getItem("number");
+  const message = localStorage.getItem("message");
+
+  // state to hold amount
+  const [amount, setAmount] = useState(0);
+
+  // state to hold alerts
+  const [alert, setAlert] = useState({
+    alert: false,
+    message: "",
+  });
 
   const handleMessageToggle = () => {
     setMessageOpen(!messageOpen);
@@ -15,12 +38,115 @@ const Review = () => {
     navigate("/create/announcement_details");
   };
 
-  const handleNext = () => {
+  const handleCheckout = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+
+      // clear alerts
+      setAlert((prev) => {
+        return {
+          ...prev,
+          alert: false,
+          message: "",
+        };
+      });
+
+      // store details in firebase
+      let announcement = await addDoc(collection(db, "announcements"), {
+        clientName: name,
+        clientContact: contact,
+        radioId: radioId,
+        radioName: radioName,
+        categoryId: categoryId,
+        categoryName: categoryName,
+        numberOfBroadcasts: number,
+        message: message,
+        amount: amount,
+        createdAt: new Date().toISOString(),
+        transactionStatus: "pending",
+      });
+
+      localStorage.setItem("announcementId", announcement.id);
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setAlert((prev) => {
+        return {
+          ...prev,
+          alert: true,
+          message: (
+            <span>
+              An error occured while processing your request, please try again
+              later or contact our support team
+            </span>
+          ),
+        };
+      });
+    }
     navigate("/create/checkout");
   };
   const handleBack = () => {
     navigate("/create/announcement_details");
   };
+
+  useEffect(() => {
+    if (
+      name == null ||
+      contact == null ||
+      radioId == null ||
+      radioName == null ||
+      categoryId == null ||
+      categoryName == null ||
+      number == null ||
+      message == null
+    ) {
+      setAlert((prev) => {
+        return {
+          ...prev,
+          alert: true,
+          message: (
+            <span>
+              Unforunately, we are not able to recover some of the data you
+              entered, you will be redirected to edit or
+              <Link to="/create" className="">
+                {" "}
+                click here to edit
+              </Link>
+            </span>
+          ),
+        };
+      });
+      // redirect after 15 seconds
+      setTimeout(() => {
+        setAlert((prev) => {
+          return {
+            ...prev,
+            alert: false,
+            message: "",
+          };
+        });
+        navigate("/create");
+      }, 15000);
+    }
+
+    if (categoryId == 1) {
+      setAmount(number * 5000);
+    }
+
+    if (categoryId == 2) {
+      setAmount(number * 7000);
+    }
+
+    if (categoryId == 3) {
+      setAmount(number * 10000);
+    }
+  }, []);
+
+  if (loading) return <Spinner />;
 
   return (
     <div className="border rounded m-2">
@@ -31,23 +157,38 @@ const Review = () => {
         <span>3 of 3</span>
       </div>
 
+      {alert.alert === true ? (
+        <div
+          className="alert alert-danger alert-dismissible fade show mx-3"
+          role="alert"
+        >
+          {alert.message}
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          ></button>
+        </div>
+      ) : null}
+
       <div className="p-3 my-3">
         {/* name of radio station */}
         <div className="bg-light p-3 rounded d-flex justify-content-between">
           <span>Radio</span>
-          <span>Unity FM</span>
+          <span>{radioName}</span>
         </div>
 
         {/* category of announcement */}
         <div className="p-3 d-flex justify-content-between">
           <span>Category</span>
-          <span>Business</span>
+          <span>{categoryName}</span>
         </div>
 
         {/* number of broadcasts */}
         <div className="bg-light p-3 rounded d-flex justify-content-between">
           <span>Number of Broadcasts</span>
-          <span>3</span>
+          <span>{number}</span>
         </div>
 
         {/* message */}
@@ -64,9 +205,7 @@ const Review = () => {
           </div>
           {messageOpen === true ? (
             <div className="p-3 bg-light rounded mb-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-              recusandae ex porro voluptatibus, deserunt harum culpa sed
-              excepturi nihil placeat?
+              {message}
               <div>
                 <button
                   className="btn btn-primary mt-3"
@@ -85,7 +224,7 @@ const Review = () => {
           <div>
             <span className="text-muted">UGX </span>
             <strong>
-              <span>10,000</span>
+              <span>{amount.toLocaleString("en-US")}</span>
             </strong>
           </div>
         </div>
@@ -97,7 +236,10 @@ const Review = () => {
           >
             Back
           </button>
-          <button className="btn btn-primary" onClick={() => handleNext()}>
+          <button
+            className="btn btn-primary"
+            onClick={(e) => handleCheckout(e)}
+          >
             Checkout
           </button>
         </div>
