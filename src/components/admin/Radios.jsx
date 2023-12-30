3692a056179d83d4d10b291667a891d8bb752025
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-12-30 05:31:29 +0300

    Add radio station list and loading spinner

--- src/components/admin/Radios.jsx
+++ src/components/admin/Radios.jsx
@@ -1,31 +1,93 @@
+import { collection, getDocs } from "firebase/firestore";
 import { Link } from "react-router-dom";
+import { db } from "../../config/firebase";
+import { useState, useEffect } from "react";
+import Spinner from "../Spinner";
 
 const Radios = () => {
+  const [radios, setRadios] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchRadios = async () => {
+      const response = await getDocs(collection(db, "radioStations"));
+      setRadios([]);
+      response.docs.forEach((doc) => {
+        let radio = {
+          id: doc.id,
+          ...doc.data(),
+        };
+        setRadios((prev) => [...prev, radio]);
+      });
+      setLoading(false);
+    };
+
+    fetchRadios();
+    return () => {
+      setRadios([]);
+      setLoading(true);
+    };
+  }, []);
+
+  if (loading) return <Spinner />;
   return (
     <div className="mx-5 my-3">
       <div className="d-flex justify-content-between">
         <h2 className="text-muted fs-3">Radios</h2>
-        <div className="w-50">
-          <input
-            className="form-control w-100"
-            list="datalistOptions"
-            id="radioDataList"
-            placeholder="Type to search..."
-          />
-          <datalist id="datalistOptions">
-            <option value="San Francisco" />
-          </datalist>
+        <div>
+          <Link to="/admin/add_radio">
+            <button className="btn btn-primary">Add Radio Station</button>
+          </Link>
         </div>
       </div>
 
-      <div className="d-flex justify-content-center align-items-center flex-column mt-5">
-        <i className="bi bi-search icon-lg"></i>
+      {radios.length === 0 ? (
+        <div className="d-flex justify-content-center align-items-center flex-column mt-5">
+          <i className="bi bi-search icon-lg"></i>
 
-        <p className="">Unfortunately we did not find any radio station</p>
-        <button className="btn bg-primary-color m-3">
-          <Link to="/admin/add_radio">Add Radio Station</Link>
-        </button>
-      </div>
+          <p className="">Unfortunately we did not find any radio station</p>
+          <button className="btn bg-primary-color m-3">
+            <Link to="/admin/add_radio">Add Radio Station</Link>
+          </button>
+        </div>
+      ) : (
+        <div className="border rounded mt-3 p-3">
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Frequency</th>
+                <th scope="col">Email</th>
+                <th scope="col">District</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {radios.map((radio, i) => {
+                return (
+                  <tr key={i}>
+                    <th scope="row">{i + 1}</th>
+                    <td>{radio.name}</td>
+                    <td>{radio.frequency}</td>
+                    <td>{radio.email}</td>
+                    <td>{radio.district}</td>
+                    <td>
+                      <span
+                        className={`badge ${
+                          radio.status === "active" ? "bg-success" : "bg-danger"
+                        }`}
+                      >
+                        {radio.status}
+                      </span>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
