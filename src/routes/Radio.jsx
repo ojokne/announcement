0ca8faec585877a08b27fe2f919d4c707ee59699
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-01-06 15:29:11 +0300

    Add routes for wrapper, users and add_user in Radio component

--- src/routes/Radio.jsx
+++ src/routes/Radio.jsx
@@ -1,12 +1,17 @@
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import Dashboard from "../components/radio/Dashboard";
+import Wrapper from "../components/radio/Wrapper";
+import Users from "../components/radio/Users";
+import AddUser from "../components/radio/AddUser";
 
 const Radio = () => {
   return (
     <Routes>
-      <Route path="/radio">
+      <Route path="/radio" element={<Wrapper />}>
         <Route index element={<Dashboard />} />
+        <Route path="users" element={<Users />} />
+        <Route path="add_user" element={<AddUser />} />
       </Route>
     </Routes>
   );
