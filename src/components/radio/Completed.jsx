d7f1b41350e12fd2a956bd6100ef575f88f528f5
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-01-06 18:12:31 +0300

    Add Completed component for displaying announcements that have been broadcast

---
+++ src/components/radio/Completed.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+/**
+ *
+ * @returns announcements in the database for the radio station that have been broadcast
+ *
+ *
+ */
+const Completed = ({ completed }) => {
+  return (
+    <div>
+      {completed.length === 0 ? (
+        <div className="d-flex justify-content-center align-items-center flex-column mt-3 p-5 border rounded">
+          <i className="bi bi-search icon-lg"></i>
+
+          <p className="">
+            Unfortunately, there are no announcements marked as complete
+          </p>
+          <Link to="/radio">
+            <button className="btn btn-primary m-3">Dashboard</button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          {completed.map((announcement, index) => {
+            return <AnnouncementSummary key={index} completed={announcement} />;
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Completed;
