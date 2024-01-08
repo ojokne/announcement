e3b272b5baeec1425cf2ad52bbf20bec82bfa3f4
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-01-08 09:44:43 +0300

    Update import paths
    This was done becuase the components were moved to a new directory

--- src/routes/General.jsx
+++ src/routes/General.jsx
@@ -1,6 +1,6 @@
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
-import Login from "../components/Login";
+import Login from "../components/general/Login";
 
 const Others = () => {
   return (
