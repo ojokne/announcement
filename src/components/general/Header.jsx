891c8a7b8f6ead649b83127b9e1f5a8aecfff475
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-02-09 06:35:07 +0300

    Add a menu for small screens

--- src/components/general/Header.jsx
+++ src/components/general/Header.jsx
@@ -1,31 +1,96 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/kakebe-logo.png";
+import { useState } from "react";
 
 const Header = () => {
+  const [style, setStyle] = useState("");
+
+  // function to show the menu
+  const showMenu = () => {
+    setStyle("showSmallMenuItems");
+  };
+
+  // function to hide the menu
+  const hideMenu = () => {
+    setStyle("hideSmallMenuItems");
+  };
   return (
     <div className="bg-dark p-2">
       <div className="container">
-        <div className="d-flex justify-content-between align-items-center">
-          <div className="d-flex align-items-center">
-            <img
-              src={logo}
-              className="rounded-circle"
-              alt="Kakebe logo"
-              width={40}
-            />
+        {/* header large screens */}
+        <div className="d-none d-lg-block">
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="d-flex align-items-center">
+              <img
+                src={logo}
+                className="rounded-circle"
+                alt="Kakebe logo"
+                width={40}
+              />
 
-            <h1 className="lead ms-2 text-white">Kakebe</h1>
+              <h1 className="lead ms-2 text-white">Kakebe</h1>
+            </div>
+            <div>
+              <Link to="/login" className="px-3 text-white">
+                Login
+              </Link>
+              <Link to="/about" className="px-3 text-white">
+                About
+              </Link>
+              <Link to="/contact" className="px-3 text-white">
+                Contact
+              </Link>
+            </div>
           </div>
-          <div>
-            <Link to="/login" className="px-3 text-white">
-              Login
-            </Link>
-            <Link to="/about" className="px-3 text-white">
-              About
-            </Link>
-            <Link to="/contact" className="px-3 text-white">
-              Contact
-            </Link>
+        </div>
+
+        {/* header for small screesn */}
+        <div className="d-lg-none">
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="d-flex align-items-center">
+              <img
+                src={logo}
+                className="rounded-circle"
+                alt="Kakebe logo"
+                width={40}
+              />
+
+              <h1 className="lead ms-2 text-white">Kakebe</h1>
+            </div>
+
+            <div onClick={() => showMenu()}>
+              <span>
+                <i className="bi bi-list text-white fs-1"></i>
+              </span>
+            </div>
+
+            {/* small screen menu items */}
+            <div className={`d-flex flex-column  bg-black p-3 smallMenuItems ${style}`}>
+              <div className="d-flex justify-content-between">
+                <div className="d-flex align-items-center">
+                  <img
+                    src={logo}
+                    className="rounded-circle"
+                    alt="Kakebe logo"
+                    width={40}
+                  />
+
+                  <h1 className="lead ms-2 text-white">Kakebe Technologies</h1>
+                </div>
+                <span onClick={() => hideMenu()}>
+                  <i className="bi bi-x-lg fs-1 text-white"></i>
+                </span>
+              </div>
+              <Link to="/login" className="p-3 text-white">
+                Login
+              </Link>
+              <Link to="/about" className="p-3 text-white">
+                About
+              </Link>
+              <Link to="/contact" className="p-3 text-white">
+                Contact
+              </Link>
+            </div>
           </div>
         </div>
       </div>
