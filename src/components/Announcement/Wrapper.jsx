7e8b342e86ca6c202016f36c33552c058e7c5f13
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-02-09 05:39:39 +0300

    Add header and footer to announcement wrapper

--- src/components/Announcement/Wrapper.jsx
+++ src/components/Announcement/Wrapper.jsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Header from "../general/Header";
+import Footer from "../general/Footer";
 
 const Wrapper = () => {
   return (
-    <div className="announcementWrapper mt-4">
-      <Outlet />
+    <div className="d-flex flex-column vh-100">
+      <div className="flex-grow-1">
+        <Header />
+        <div className="announcementWrapper mt-4">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
