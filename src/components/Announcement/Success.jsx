54eccaf2cec9cb0aeef99e5b88c806b1252c31fb
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-02-09 05:59:00 +0300

    Create and add success component
    The user is redirected to this page if the payment was successfull

---
+++ src/components/Announcement/Success.jsx
@@ -0,0 +1,23 @@
+import Footer from "../general/Footer";
+import Header from "../general/Header";
+
+const Success = () => {
+  return (
+    <div className="d-flex flex-column vh-100">
+      <div className="flex-grow-1">
+        <Header />
+        <div className="announcementWrapper mt-4">
+          <div className="bg-white p-3 m-3 rounded shadow-sm d-flex justify-content-center align-items-center flex-column">
+            <span>
+              <i className="bi bi-check-circle text-success fs-1"></i>
+            </span>
+            <span className="text-muted">Your payment was successful</span>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Success;
--- src/routes/General.jsx
+++ src/routes/General.jsx
@@ -2,12 +2,14 @@ import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import Login from "../components/general/Login";
 import Cancelled from "../components/Announcement/Cancelled";
+import Success from "../components/Announcement/Success";
 
 const General = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/cancelled" element={<Cancelled />} />
+      <Route path="/success" element={<Success />} />
     </Routes>
   );
 };
