7592b74eb088db4eb562d6a1c1e4945eed25dd28
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-02-09 05:55:18 +0300

    Create and add cancelled component
    The user is redirected to this page if he cancels the payment

---
+++ src/components/Announcement/Cancelled.jsx
@@ -0,0 +1,23 @@
+import Footer from "../general/Footer";
+import Header from "../general/Header";
+
+const Cancelled = () => {
+  return (
+    <div className="d-flex flex-column vh-100">
+      <div className="flex-grow-1">
+        <Header />
+        <div className="announcementWrapper mt-4">
+          <div className="bg-white p-3 m-3 rounded shadow-sm d-flex justify-content-center align-items-center flex-column">
+            <span>
+              <i className="bi bi-x-circle text-danger fs-1"></i>
+            </span>
+            <span className="text-muted">Your payment was cancelled</span>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Cancelled;
--- src/routes/General.jsx
+++ src/routes/General.jsx
@@ -1,11 +1,13 @@
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import Login from "../components/general/Login";
+import Cancelled from "../components/Announcement/Cancelled";
 
 const General = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/cancelled" element={<Cancelled />} />
     </Routes>
   );
 };
