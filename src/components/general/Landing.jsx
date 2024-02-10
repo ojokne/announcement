6a2882223d89006bacb6f59730327bd9308dff50
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-02-10 08:25:10 +0300

    Modify landing page

--- src/components/general/Landing.jsx
+++ src/components/general/Landing.jsx
@@ -8,19 +8,23 @@ const Landing = () => {
       <div className="fixed-top">
         <Header />
       </div>
-      <div className="landing-page">
-        <div className="landing-page-overlay">
-          <div className="landing-page-content">
-            <h1 className="mx-2">Empowering Voices, Amplifying Reach</h1>
-            <Link to="/create">
-              <button className="btn btn-danger text-white btn-lg">
-                Send Announcment
-              </button>
-            </Link>
+      <div className="d-flex flex-column vh-100">
+        <div className="flex-grow-1">
+          <div className="landing-page">
+            <div className="landing-page-overlay">
+              <div className="landing-page-content">
+                <h1 className="mx-2">Empowering Voices, Amplifying Reach</h1>
+                <Link to="/create">
+                  <button className="btn btn-danger text-white btn-lg">
+                    Send Announcment
+                  </button>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 };
