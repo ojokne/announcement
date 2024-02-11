0376ef2add1e065dab503d383cdd9d487aa71ee7
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-02-11 06:42:59 +0300

    Remove margin from footer

--- src/components/general/Footer.jsx
+++ src/components/general/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <div className="bg-black">
+    <div className="bg-black" style={{ marginBottom: 0,padding:0 }}>
       <div className="container text-white">
-        <div className="p-2 m-2 text-center">
-          <p> &copy; Kakebe Technologies {new Date().getFullYear()}</p>
+        <div className="p-2 text-center">
+          <p className="m-3 p-3"> &copy; Kakebe Technologies {new Date().getFullYear()}</p>
         </div>
       </div>
     </div>
