8ecf86fb8f4addd1c45067472d4491eba600e305
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2024-02-11 15:12:39 +0300

    Add link to WhatsApp

--- src/components/general/Footer.jsx
+++ src/components/general/Footer.jsx
@@ -1,9 +1,17 @@
 const Footer = () => {
   return (
-    <div className="bg-black" style={{ marginBottom: 0,padding:0 }}>
+    <div className="bg-black" style={{ marginBottom: 0, padding: 0 }}>
       <div className="container text-white">
+        <div className="d-flex justify-content-center align-items-center flex-column pt-3">
+          <span className="mb-2">WhatsApp</span>
+          <span className="my-1">
+            <a href="https://wa.me/256777676206">
+              <i className="bi bi-whatsapp text-white"></i>
+            </a>
+          </span>
+        </div>
         <div className="p-2 text-center">
-          <p className="m-3 p-3"> &copy; Kakebe Technologies {new Date().getFullYear()}</p>
+          <p> &copy; Kakebe Technologies {new Date().getFullYear()}</p>
         </div>
       </div>
     </div>
